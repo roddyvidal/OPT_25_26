25cee84b88b953b7aca4109c9d50e1787d58aeb8
roddyvidal <vidalsanchezroddy7@gmail.com>
2025-09-22 18:14:52 +0200

    add

--- 2_calculadora_simple.py
+++ 2_calculadora_simple.py
@@ -1,6 +1,23 @@
 
-print(10+5)
-print(10-5)
-print(10*5)
-print(10/5)
-print(10%5)
+num1 = float(input("Pon tu primer número: "))
+operator = input("Elige que operador vas a usar (+, -, *, /, %): ")
+num2 = float(input("Pon tu segundo número"))
+
+if operator == "+":
+    result = num1 + num2
+elif operator == "-":
+    result = num1 - num2
+elif operator == "*":
+    result = num1 * num2
+elif operator == "%":
+        result = num1 % num2
+elif operator == "/":
+    if num2 != 0:
+        result = num1 / num2
+    else:  result = "No se puede dividir entre 0"
+else:  result = "No es un operador válido"
+
+#Mostrar resultado
+print("El resultado de la operación es: ", result)
+
+
